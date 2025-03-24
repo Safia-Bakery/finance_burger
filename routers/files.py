15da5d974d3dee6ff4404e0b9dc673fabdeabc0e
Bakhtiyor Bakhriddinov <dataeng27@gmail.com>
2025-03-24 12:21:12 +0500

    Added changes

--- routers/files.py
+++ routers/files.py
@@ -7,6 +7,15 @@ from fastapi import Depends, File
 
 from utils.utils import PermissionChecker
 
+from fastapi import FastAPI, Request, HTTPException, status
+from streaming_form_data import StreamingFormDataParser
+from streaming_form_data.targets import FileTarget, ValueTarget
+from streaming_form_data.validators import MaxSizeValidator
+import streaming_form_data
+from starlette.requests import ClientDisconnect
+from urllib.parse import unquote
+import os
+
 files_router = APIRouter()
 
 
@@ -60,3 +69,66 @@ async def upload_bot_files(
     file_paths.append(file_path)
 
     return {"file_paths": file_paths}
+
+
+MAX_FILE_SIZE = 1024 * 1024 * 1024 * 4  # = 4GB
+MAX_REQUEST_BODY_SIZE = MAX_FILE_SIZE + 1024
+
+
+class MaxBodySizeException(Exception):
+    def __init__(self, body_len: str):
+        self.body_len = body_len
+
+class MaxBodySizeValidator:
+    def __init__(self, max_size: int):
+        self.body_len = 0
+        self.max_size = max_size
+
+    def __call__(self, chunk: bytes):
+        self.body_len += len(chunk)
+        if self.body_len > self.max_size:
+            raise MaxBodySizeException(body_len=self.body_len)
+
+
+@files_router.post('/files/upload/bot/2')
+async def upload(request: Request):
+    body_validator = MaxBodySizeValidator(MAX_REQUEST_BODY_SIZE)
+    filename = request.headers.get('filename')
+    base_dir = "files"
+    date_dir = datetime.now().strftime("%Y/%m/%d")  # Create a path like "2025/03/10"
+    save_dir = os.path.join(base_dir, date_dir)
+
+    if not filename:
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                            detail='Filename header is missing')
+    try:
+        filename = unquote(filename)
+        filepath = os.path.join(save_dir, os.path.basename(filename))
+        file_ = FileTarget(filepath, validator=MaxSizeValidator(MAX_FILE_SIZE))
+        data = ValueTarget()
+        parser = StreamingFormDataParser(headers=request.headers)
+        parser.register('file', file_)
+        parser.register('data', data)
+
+        async for chunk in request.stream():
+            body_validator(chunk)
+            parser.data_received(chunk)
+    except ClientDisconnect:
+        print("Client Disconnected")
+    except MaxBodySizeException as e:
+        raise HTTPException(status_code=status.HTTP_413_REQUEST_ENTITY_TOO_LARGE,
+                            detail=f'Maximum request body size limit ({MAX_REQUEST_BODY_SIZE} bytes) exceeded ({e.body_len} bytes read)')
+    except streaming_form_data.validators.ValidationError:
+        raise HTTPException(status_code=status.HTTP_413_REQUEST_ENTITY_TOO_LARGE,
+                            detail=f'Maximum file size limit ({MAX_FILE_SIZE} bytes) exceeded')
+    except Exception:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail='There was an error uploading the file')
+
+    if not file_.multipart_filename:
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail='File is missing')
+
+    print(data.value.decode())
+    print(file_.multipart_filename)
+
+    return {"message": f"Successfuly uploaded {filename}"}
