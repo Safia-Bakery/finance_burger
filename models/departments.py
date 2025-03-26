5caf329ab87f017ebe075bbfc00619dbc4be6332
Bakhtiyor Bakhriddinov <dataeng27@gmail.com>
2025-03-26 13:54:29 +0500

    Removed checking permission for Statistics endpoint

--- models/departments.py
+++ models/departments.py
@@ -13,7 +13,7 @@ class Departments(Base):
     id = Column(UUID, primary_key=True, default=uuid.uuid4)
     name = Column(String, nullable=False)
     is_active = Column(Boolean, default=True)
-    user_id = Column(UUID, ForeignKey("users.id", ondelete="SET NULL"), unique=True, nullable=True)
+    client_id = Column(UUID, ForeignKey("clients.id", ondelete="SET NULL"), unique=True, nullable=True)
     head = relationship('Clients', back_populates='department') # lazy="selectin"
     requests = relationship('Requests', back_populates='department', passive_deletes=True) # lazy='select'
     created_at = Column(DateTime(timezone=True), default=func.now())
