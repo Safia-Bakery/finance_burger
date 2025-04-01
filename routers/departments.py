ebd51aa4ce1e2872add752a281ff365b3a7a5301
Bakhtiyor Bakhriddinov <dataeng27@gmail.com>
2025-04-01 14:28:26 +0500

    fixed roles endpoints

--- routers/departments.py
+++ routers/departments.py
@@ -39,7 +39,7 @@ async def get_department_list(
 
     departments = await DepartmentDAO.get_by_attributes(session=db, filters=filters if filters else None)
     user = await UserDAO.get_by_attributes(session=db, filters={"id": current_user["id"]}, first=True)
-    role_department_relations = user.role.departments
+    role_department_relations = user.role.roles_departments
     role_departments = [relation.department_id for relation in role_department_relations]
     departments = [department for department in departments if department.id in role_departments] if role_departments else [department for department in departments]
     for department in departments:
